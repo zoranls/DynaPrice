102fbdbf113bb5cfea333b8e8bb24a88a0d15844
Zoki <80131781+zoranls@users.noreply.github.com>
2024-05-16 10:49:35 +0200

    Update Program.cs

--- DynaPrice/Program.cs
+++ DynaPrice/Program.cs
@@ -7,17 +7,33 @@ namespace DynaPrice
         static void Main(string[] args)
         {
             Product product = new Product("Zlato", "1935", 20.25m, "USD");
-                
+            void a(){
+            Console.WriteLine("Unesite Ime proizvoda:");
+            product.Name = Console.ReadLine();
+            Console.WriteLine("Unesite UPC proizvoda:");
+            product.UPC = Console.ReadLine();
+            Console.WriteLine("Unesite Cenu proizvoda:");
+            product.Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesite popust proizvoda:");
+             try
+             {
+                a();
+                 
+             }
+             catch (System.Exception e)
+             {
+                 Console.WriteLine($"Pogresan input {e}");
+                 throw;
+             }   
+            decimal discountPercentage = Convert.ToDecimal(Console.ReadLine()); 
+            
             //Generate report with Tax (20%) and NO discount
             decimal taxPercentage = 20;
-            decimal discountPercentage = 15;
             ReportGenerator.GenerateReport(product, taxPercentage, discountPercentage);
 
             //Generate report with Tax (20%) and 15% discount
-            taxPercentage = 20;
-            discountPercentage = 0;
             ReportGenerator.GenerateReport(product, taxPercentage, discountPercentage);
 
         }
     }
-}
+}}
